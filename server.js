9edb6664a648cc2a395f8028149411792c2bcbf4
sellmc2026 <sellmc2026@gmail.com>
2026-08-28 22:48:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ io.on("connection", (socket) => {
 });
 
 
-server.listen(3000, () => {
-    console.log("Message server running on port 3000");
+server.listen(process.env.PORT || 3000, () => {
+    console.log("Message server is running");
 });
